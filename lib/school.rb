cca94dde0a249daf9fd0e6064963c68216a9271d
Anthea Merton <anthea.merton@gmail.com>
2019-04-04 19:46:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student=(name,grade)
+    @name = name
+    @roster = roster
+end
+
+def add_student(name, grade)
+  @name
+  @grade
+end
